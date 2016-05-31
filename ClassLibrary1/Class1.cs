dd73d5cb943144941ec66017119b42714aeac88d
vika0392 <vika03_92@mail.ru>
2016-05-31 18:41:42 +0500

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using System.Data;  
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public String Test()
+        {
+            string oradb = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.1.164)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=IPSTest))); User Id=time_tracker;Password=time1234;";
+            OracleConnection conn = new OracleConnection(oradb);
+            conn.Open();
+            string sql = "select  count(*) as cnt from position"; 
+            OracleCommand cmd = new OracleCommand(sql, conn);
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            String test = dr["cnt"].ToString();
+            conn.Close();
+            return test;
+        }
+    }
+}
