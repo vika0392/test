cd3068c77d248c6cc77b0eb0c078c057124593ba
vika0392 <vika03_92@mail.ru>
2016-05-31 18:42:53 +0500

    Add files via upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cl = new Class1();
+            Console.WriteLine(cl.Test());
+            Console.ReadLine();
+        }
+    }
+}
